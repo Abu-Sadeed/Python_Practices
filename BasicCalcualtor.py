21dfde7d240684c03b1ca64a86e5c8768615196e
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2019-11-08 22:18:05 +0600

    Practiced with numbers & Strings

---
+++ BasicCalcualtor.py
@@ -0,0 +1,14 @@
+num1 = input("Enter a Number: ")
+num2 = input("Enter another Number: ")
+
+add = float(num1) + float(num2)
+sub = float(num1) - float(num2)
+mul = float(num1) * float(num2)
+div = float(num1) / float(num2)
+mod = float(num1) % float(num2)
+
+print("Addition = " + str(add))
+print("Subtraction = " + str(sub))
+print("Multiplication = " + str(mul))
+print("Division = " + str(div))
+print("Modulus = " + str(mod))
